a0f2bb1b5a436844b7a4979d958fc4f3224b91f6
J.E. Flietstra <jeroen.flietstra@hva.nl>
2020-12-16 14:49:28 +0100

    Added test for user stories: * US-5 – Diagrams: Save diagram

--- umlet-swing/src/test/java/com/baselet/diagram/io/DiagramFileHandlerTest.java
+++ umlet-swing/src/test/java/com/baselet/diagram/io/DiagramFileHandlerTest.java
@@ -1,82 +1,45 @@
 package com.baselet.diagram.io;
 
-import com.baselet.control.constants.Constants;
 import com.baselet.control.enums.Program;
 import com.baselet.control.enums.RuntimeType;
-import com.baselet.diagram.CurrentDiagram;
 import com.baselet.diagram.DiagramHandler;
-import com.baselet.diagram.DrawPanel;
-import com.baselet.element.interfaces.Diagram;
-import com.baselet.gui.BaseGUI;
-import com.baselet.gui.CurrentGui;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-import org.mockito.Spy;
-import javax.swing.*;
-import java.io.File;
-import java.io.IOException;
-import java.awt.Frame;
-import java.lang.reflect.Field;
 
-import static org.junit.Assert.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static org.junit.Assert.assertTrue;
 
 public class DiagramFileHandlerTest {
 
-    private Frame frame;
     private DiagramHandler diagramHandler;
-    private JFileChooser fileChooser;
-    private DiagramFileHandler handler;
-    private BaseGUI baseGUI;
-    private String absoluteTestDir;
+    private DiagramFileHandler fileHandler;
+    private File file;
 
     @Before
-    public void setUp(){
-        String path = "src/test/resources";
-        File file = new File(path);
-        absoluteTestDir = file.getAbsolutePath();
-        frame = Mockito.mock(Frame.class);
-        fileChooser = Mockito.mock(JFileChooser.class);
-        diagramHandler = Mockito.mock(DiagramHandler.class);
-        baseGUI = Mockito.mock(BaseGUI.class);
-        diagramHandler.setChanged(true);
+    public void setUp() throws FileNotFoundException {
         Program.init("", RuntimeType.STANDALONE);
-
-        Mockito.when(fileChooser.showSaveDialog(frame)).thenReturn(JFileChooser.APPROVE_OPTION);
-        Mockito.when(fileChooser.getSelectedFile()).thenReturn(new File(absoluteTestDir + "\\test"));
-        Mockito.when(diagramHandler.getName()).thenReturn("test");
-        Mockito.when(diagramHandler.getDrawPanel()).thenReturn(new DrawPanel(diagramHandler, true));
-        Mockito.when(diagramHandler.getGridSize()).thenReturn(Constants.DEFAULTGRIDSIZE); // Prevent arithmetic error (division by zero)
-        CurrentGui.getInstance().setGui(baseGUI);
-        Mockito.when(baseGUI.getMainFrame()).thenReturn(frame);
-        CurrentDiagram.getInstance().setCurrentDiagramHandler(diagramHandler);
-        CurrentGui.getInstance().setGui(baseGUI);
-        Mockito.when(baseGUI.getMainFrame()).thenReturn(frame);
-        handler = new DiagramFileHandler(diagramHandler,null);
+        String fileName = "test";
+        file = new File("src/test/resources/" + fileName + "." + Program.getInstance().getExtension());
+        diagramHandler = Mockito.mock(DiagramHandler.class);
+        fileHandler = new DiagramFileHandler(diagramHandler, file);
+        new FileOutputStream(file);
     }
 
-
     @Test
-    public void GivenModifiedCanvas_WhenCanvasNotSaved_ThenSaveDialogShows() throws IOException {
-        //Given
+    public void GivenDiagramChanged_WhenSaveDiagram_ThenDiagramSavedOnDrive() throws IOException {
+        // Given
         diagramHandler.setChanged(true);
-        //When
-        handler = new DiagramFileHandler(diagramHandler,null);
-        handler.doSave();
-        //Then
-        assertTrue(true);
+        long currentTime = System.currentTimeMillis();
+
+        // When
+        fileHandler.doSave();
+
+        // Then
+        assertTrue(file.lastModified() > currentTime);
     }
-
-    @Test
-    public void GivenModifiedCanvas_WhenCanvasAlreadySavedOnce_ThenFileSaves() throws IOException{
-        handler = new DiagramFileHandler(diagramHandler,new File(absoluteTestDir + "\\test.ufx"));
-
-        handler.doSave();
-
-    }
-
 }
