a4141bcfa298465116be7b01e94244268bc9695e
J.E. Flietstra <jeroen.flietstra@hva.nl>
2020-12-16 14:52:26 +0100

    minor change

--- umlet-swing/src/test/java/com/baselet/diagram/io/DiagramFileHandlerTest.java
+++ umlet-swing/src/test/java/com/baselet/diagram/io/DiagramFileHandlerTest.java
@@ -8,7 +8,6 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -21,18 +20,18 @@ public class DiagramFileHandlerTest {
     private File file;
 
     @Before
-    public void setUp() throws FileNotFoundException {
+    public void setUp() {
         Program.init("", RuntimeType.STANDALONE);
         String fileName = "test";
         file = new File("src/test/resources/" + fileName + "." + Program.getInstance().getExtension());
         diagramHandler = Mockito.mock(DiagramHandler.class);
         fileHandler = new DiagramFileHandler(diagramHandler, file);
-        new FileOutputStream(file);
     }
 
     @Test
-    public void GivenDiagramChanged_WhenSaveDiagram_ThenDiagramSavedOnDrive() throws IOException {
+    public void GivenExistingDiagramChanged_WhenSaveDiagram_ThenDiagramSavedOnDrive() throws IOException {
         // Given
+        new FileOutputStream(file); // File exists.
         diagramHandler.setChanged(true);
         long currentTime = System.currentTimeMillis();
 
